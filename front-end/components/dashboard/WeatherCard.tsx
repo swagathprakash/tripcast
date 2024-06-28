eff980e3011a19e9c96a04afac12db8c45500921
aishwarya.prabhu <aishwarya.prabhu@qburst.com>
2024-06-28 12:41:19 +0530

    feat: weather card ui

--- front-end/components/dashboard/WeatherCard.tsx
+++ front-end/components/dashboard/WeatherCard.tsx
@@ -1,56 +1,80 @@
-import { View, Text, ImageBackground } from "react-native";
-import React from "react";
-import { images } from "@/constants";
+import { View, Text, ImageBackground, ActivityIndicator, Image, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { icons, images } from "@/constants";
 import { opacity } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
+import { setCategory as setCategorySlice } from "@/store/slice/locationSlice";
+import { useAppDispatch, useAppSelector } from "@/store";
+import locationSlice from "@/store/slice/locationSlice";
+import { getCategory } from "@/libs/utils";
 
 const WeatherCard = () => {
+  const dispatch = useAppDispatch();
+  const { weather, loading, location, category } = useAppSelector((state) => state.location);
   const data = [
-    { label: "Rain", value: "22%" },
-    { label: "Wind speed", value: "12 km/h" },
-    { label: "Humidity", value: "22%" },
+    { label: "Rain", value: (!loading && weather?.rain) ? `${weather.rain} %` : "0 %" },
+    { label: "Wind speed", value: (!loading && weather?.windspeed) ? `${weather.windspeed} km/hr` : "" },
+    { label: "Humidity", value: (!loading && weather?.relativehumidity) ? `${weather.relativehumidity} %` : "" },
   ];
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
+  useEffect(() => {
+    !loading && weather && dispatch(setCategorySlice(getCategory(weather?.weathercode)))
+  }, [weather?.weathercode, loading, weather]);
+
+  const textColor = loading ? 'text-black' : category === 'Cloudy' ? 'text-white' : category === 'Sunny' ? 'text-white' : category === 'Rainy' ? 'text-white' : 'text-white';
   return (
-    <View className="bg-white flex-1 overflow-hidden rounded-md shadow-md shadow-gray-900 mx-2 my-5">
+    <View className="bg-white flex-1 overflow-hidden rounded-md shadow-md shadow-gray-900 mx-2 my-5 h-[168px]">
       <ImageBackground
-        source={images.weatherBg}
+        source={!loading ? (category === 'Cloudy' ? images.Cloudy : category === 'Rainy' ? images.Rainy : category === 'Thunderstorm' ? images.Thunderstorm : images.Sunny) : images.Sunny}
         resizeMode="cover"
-        className="p-3"
+        className="p-3 flex-1"
         imageStyle={{ opacity: 0.9 }}
       >
-        <View className="flex-row justify-between ">
-          <View>
-            <Text className="text-white font-semibold text-2xl">Today</Text>
-            <Text className="text-white font-semibold text-sm tracking-widest ">
-              Monday June 24
-            </Text>
-            <Text className="text-white font-semibold text-xs">
-              Koratty, Kerala
-            </Text>
-          </View>
-          <View className="items-center pr-2">
-            <View className="bg-blue-300 h-16 w-16"></View>
-            <Text className="text-center pt-1 text-white font-semibold text-base">
-              Mostly Cloudy
-            </Text>
-          </View>
-        </View>
-        <View className="flex-row justify-start gap-3 pt-3 pb-1">
-          {data.map((item, index) => {
-            return (
-              <View key={index}>
-                <Text className="text-white font-semibold text-sm">
-                  {item.value}
+        {loading ? <View className="my-auto"><ActivityIndicator className="my-auto" size='large' color='#1f1e1e' /><Text className="text-center">Loading weather</Text></View> : (
+          <>
+            <View className="flex-row justify-between">
+              <View>
+                <Text className={`font-semibold text-2xl ${textColor}`} style={styles.text}>Today   {weather?.temperature}°C</Text>
+                <Text className={`font-semibold text-sm tracking-widest ${textColor}`} style={styles.text}>
+                  {formattedDate}
                 </Text>
-                <Text className="text-white font-semibold text-xs">
-                  {item.label}
+                <Text className={`font-semibold text-xs ${textColor}`} style={styles.text}>
+                  {location}
                 </Text>
               </View>
-            );
-          })}
-        </View>
+              <View className="items-center pr-2">
+                <Image className="h-16 w-16" resizeMode="contain" source={category === 'Sunny' ? icons.Sunny : category === 'Cloudy' ? icons.Cloudy : category === 'Rainy' ? icons.Rainy : icons.Thunderstorm} />
+                <Text className={`text-center -top-3 font-semibold text-base ${textColor}`} style={styles.text}>
+                  {weather?.weather_detail}
+                </Text>
+              </View>
+            </View>
+            <View className="flex-row justify-start gap-3 pt-3 pb-1">
+              {data.map((item, index) => {
+                return (
+                  <View key={index}>
+                    <Text className={`font-semibold text-sm ${textColor}`} style={styles.text}>
+                      {item.value}
+                    </Text>
+                    <Text className={`font-semibold text-sm ${textColor}`} style={styles.text}>
+                      {item.label}
+                    </Text>
+                  </View>
+                );
+              })}
+            </View>
+          </>
+        )}
       </ImageBackground>
     </View>
   );
 };
 
 export default WeatherCard;
+const styles = StyleSheet.create({
+  text: {
+    textShadowColor: "#1f1e1e",
+    textShadowOffset: { height: 1, width: 1 },
+    textShadowRadius: 1,
+  }
+});
--- front-end/libs/utils.ts
+++ front-end/libs/utils.ts
@@ -23,6 +23,23 @@ const storeData = async (value: string) => {
   } catch (e) {
     // saving error
   }
-};
+}; 
+
+const categories: { [key: string]: number[] } = {
+  Sunny: [0],
+  Thunderstorm: [17, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99],
+  Rainy: [15, 16, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 80, 81, 82, 83, 84, 85],
+  Cloudy: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
+}
+
+export function getCategory(weatherCode = 0 ) {
+  let result='Cloudy';
+  for (const category in categories) {
+    if (categories[category].includes(weatherCode)) {
+      result= category;
+    }
+  }
+  return result;
+}
 
 export { validateMobileNumber, getData, storeData };
