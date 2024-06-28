37614041eb67f9f9eebdf99a631daebc07ab1589
Abhishek P V <abhishek.pottammal@qburst.com>
2024-06-29 02:08:19 +0530

    itenary details from slice

--- front-end/components/itenary/itenary-details.tsx
+++ front-end/components/itenary/itenary-details.tsx
@@ -1,4 +1,11 @@
-import { View, Text, Pressable, ScrollView, Image, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  ScrollView,
+  Image,
+  ActivityIndicator,
+} from "react-native";
 import React from "react";
 import { router } from "expo-router";
 import { getCategory } from "@/libs/utils";
@@ -6,164 +13,190 @@ import { icons } from "@/constants";
 import { useAppDispatch, useAppSelector } from "@/store";
 import { saveItinaray } from "@/store/slice/itenararySlice";
 
-const ItenaryDetails = ({ tripId }: { tripId?: string | string[] | undefined }) => {
+const ItenaryDetails = ({
+  tripId,
+}: {
+  tripId?: string | string[] | undefined;
+}) => {
   const dispatch = useAppDispatch();
   const { tripDetails, loading } = useAppSelector((state) => state.itenrary);
   const { user, loading: userLoading } = useAppSelector((state) => state.auth);
   const saveItinerary = () => {
     console.log("save", loading, userLoading);
 
-    !loading && !userLoading && user && user.user_id && tripDetails && dispatch(saveItinaray({ userId: user.user_id, tripDetails: tripDetails }))
-  }
+    !loading &&
+      !userLoading &&
+      user &&
+      user.user_id &&
+      tripDetails &&
+      dispatch(
+        saveItinaray({ userId: user.user_id, tripDetails: tripDetails })
+      );
+  };
   return (
     <>
-      {loading ? <ActivityIndicator /> : (
-        tripDetails ?
-          <View>
-            <Text className="text-4xl py-5 font-semibold text-primary px-5">
-              {tripDetails.destination}
-            </Text>
-            <View className="px-3 py-5 flex-row gap-2 justify-start flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300 ">
-              <View className=" w-36 my-2">
-                <Text className="text-xs text-gray-400 font-medium">Companion</Text>
-                <Text className="text-lg text-primary font-semibold">
-                  {tripDetails.companions}
-                </Text>
-              </View>
-              <View className=" w-36 my-2">
-                <Text className="text-xs text-gray-400 font-medium">Duration</Text>
-                <Text className="text-lg text-primary font-semibold">
-                  {tripDetails.duration} days
-                </Text>
-              </View>
-              <View className=" w-36 my-2">
-                <Text className="text-xs text-gray-400 font-medium">Purpose</Text>
-                <Text className="text-lg text-primary font-semibold">
-                  {tripDetails.purpose}
-                </Text>
-              </View>
-              <View className=" w-36 my-2">
-                <Text className="text-xs text-gray-400 font-medium">
-                  Starting location
-                </Text>
-                <Text className="text-lg text-primary font-semibold">
-                  {tripDetails.starting_location}
-                </Text>
-              </View>
+      {loading ? (
+        <ActivityIndicator />
+      ) : tripDetails ? (
+        <View>
+          <Text className="text-4xl py-5 font-semibold text-primary px-5">
+            {tripDetails.destination}
+          </Text>
+          <View className="px-3 py-5 flex-row gap-2 justify-start flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300 ">
+            <View className=" w-36 my-2">
+              <Text className="text-xs text-gray-400 font-medium">
+                Companion
+              </Text>
+              <Text className="text-lg text-primary font-semibold">
+                {tripDetails.companions}
+              </Text>
             </View>
-            <View className=" flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300">
-              <View className="border-b-[1px] border-b-gray-100 flex-row justify-between items-center py-2 px-5 w-full">
-                <Text className="text-lg text-primary font-semibold ">Weather</Text>
-              </View>
-              <View className="flex-1 overflow-hidden p-5">
-                <ScrollView horizontal className="flex-1 gap-4 pl-1 pb-[5px]">
-                  {tripDetails.forecast.map((item, index) => {
-                    const category = getCategory(item.weather_code);
-                    const day = new Date(item.time).getDate()
-                    const month = new Date(item.time).getMonth()
-                    return (
-                      <View key={index} className="items-center justify-start pr-2">
-                        <Text className="text-[10px] font-semibold text-gray-400">
-                          {day + `/` + month}
-                        </Text>
-                        <Text className="text-xs font-semibold text-primary">
-                          {item.time.split("T")[1]}
-                        </Text>
-                        <Image
-                          className="h-10 w-10"
-                          resizeMode="contain"
-                          source={
-                            category === "Sunny"
-                              ? icons.Sunny
-                              : category === "Cloudy"
-                                ? icons.Cloudy
-                                : category === "Rainy"
-                                  ? icons.Rainy
-                                  : icons.Thunderstorm
-                          }
-                        />
-                        <Text
-                          className={`text-center font-semibold text-[10px] text-gray-400 `}
-                        >
-                          {item?.weather}
-                        </Text>
-                      </View>
-                    );
-                  })}
-                </ScrollView>
-              </View>
+            <View className=" w-36 my-2">
+              <Text className="text-xs text-gray-400 font-medium">
+                Duration
+              </Text>
+              <Text className="text-lg text-primary font-semibold">
+                {tripDetails.duration} days
+              </Text>
             </View>
-            <View className=" flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300 ">
-              <View className="border-b-[1px] border-b-gray-100 flex-row justify-between items-center py-2 px-5 flex-1">
-                <Text className="text-lg text-primary font-semibold">Itenary</Text>
-              </View>
-              <View className="py-3 px-5 flex-1 w-full ">
-                <Pressable
-                  className="m-3 mb-5  bg-primary rounded-md shadow-md shadow-gray-500 justify-center py-2"
-                  onPress={() => {
-                    router.push("/itenary-day");
-                  }}
-                >
-                  <Text className="w-full text-center text-white text-lg font-bold">
-                    View Details
-                  </Text>
-                </Pressable>
-                <Pressable
-                  className="m-3 mt-0 mb-5  bg-primary rounded-md shadow-md shadow-gray-500 justify-center py-2"
-                  onPress={saveItinerary}
-                >
-                  <Text className="w-full text-center text-white text-lg font-bold">
-                    Save Itinerary
-                  </Text>
-                </Pressable>
-              </View>
+            <View className=" w-36 my-2">
+              <Text className="text-xs text-gray-400 font-medium">Purpose</Text>
+              <Text className="text-lg text-primary font-semibold">
+                {tripDetails.purpose}
+              </Text>
             </View>
-            <View className=" flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300 ">
-              <View className="border-b-[1px] border-b-gray-100 flex-row justify-between items-center py-2 px-5 flex-1">
-                <Text className="text-lg text-primary font-semibold">
-                  Packing Recommendations
-                </Text>
-              </View>
-              <View className="py-3 px-5">
-                {tripDetails.packingRecommendations.map((item, index) => {
-                  return (
-                    <View key={index} className="py-1 flex-row flex-1 items-start">
-                      <View className="rounded-full h-2 w-2 bg-gray-400 my-[5px] mr-1"></View>
-                      <Text className="text-xs text-gray-500  font-semibold ">
-                        {item}
-                      </Text>
-                    </View>
-                  );
-                })}
-              </View>
+            <View className=" w-36 my-2">
+              <Text className="text-xs text-gray-400 font-medium">
+                Starting location
+              </Text>
+              <Text className="text-lg text-primary font-semibold">
+                {tripDetails.starting_location}
+              </Text>
             </View>
-            <View className=" flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300 ">
-              <View className="border-b-[1px] border-b-gray-100 flex-row justify-between items-center py-2 px-5 flex-1">
-                <Text className="text-lg text-primary font-semibold">
-                  Safety Tips
-                </Text>
-              </View>
-              <View className="py-3 px-5">
-                {tripDetails.safetyTips.map((item, index) => {
+          </View>
+          <View className=" flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300">
+            <View className="border-b-[1px] border-b-gray-100 flex-row justify-between items-center py-2 px-5 w-full">
+              <Text className="text-lg text-primary font-semibold ">
+                Weather
+              </Text>
+            </View>
+            <View className="flex-1 overflow-hidden p-5">
+              <ScrollView horizontal className="flex-1 gap-4 pl-1 pb-[5px]">
+                {tripDetails.forecast.map((item, index) => {
+                  const category = getCategory(item.weather_code);
+                  const day = new Date(item.time).getDate();
+                  const month = new Date(item.time).getMonth();
                   return (
                     <View
                       key={index}
-                      className="py-1 flex-row flex-1 items-start mr-1"
+                      className="items-center justify-start pr-2"
                     >
-                      <View className="rounded-full h-2 w-2 bg-gray-400 my-[5px] mr-1"></View>
-                      <Text className="text-xs text-gray-500  font-semibold ">
-                        {item}
+                      <Text className="text-[10px] font-semibold text-gray-400">
+                        {day + `/` + month}
+                      </Text>
+                      <Text className="text-xs font-semibold text-primary">
+                        {item.time.split("T")[1]}
+                      </Text>
+                      <Image
+                        className="h-10 w-10"
+                        resizeMode="contain"
+                        source={
+                          category === "Sunny"
+                            ? icons.Sunny
+                            : category === "Cloudy"
+                            ? icons.Cloudy
+                            : category === "Rainy"
+                            ? icons.Rainy
+                            : icons.Thunderstorm
+                        }
+                      />
+                      <Text
+                        className={`text-center font-semibold text-[10px] text-gray-400 `}
+                      >
+                        {item?.weather}
                       </Text>
                     </View>
                   );
                 })}
-              </View>
+              </ScrollView>
             </View>
-          </View> : (
-            <View>
-              <Text>Trip details is not available</Text>
+          </View>
+          <View className=" flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300 ">
+            <View className="border-b-[1px] border-b-gray-100 flex-row justify-between items-center py-2 px-5 flex-1">
+              <Text className="text-lg text-primary font-semibold">
+                Itenary
+              </Text>
             </View>
-          )
+            <View className="py-3 px-5 flex-1 w-full ">
+              <Pressable
+                className="m-3 mb-5  bg-primary rounded-md shadow-md shadow-gray-500 justify-center py-2"
+                onPress={() => {
+                  router.push("/itenary-day");
+                }}
+              >
+                <Text className="w-full text-center text-white text-lg font-bold">
+                  View Details
+                </Text>
+              </Pressable>
+              <Pressable
+                className="m-3 mt-0 mb-5  bg-primary rounded-md shadow-md shadow-gray-500 justify-center py-2"
+                onPress={saveItinerary}
+              >
+                <Text className="w-full text-center text-white text-lg font-bold">
+                  Save Itinerary
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+          <View className=" flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300 ">
+            <View className="border-b-[1px] border-b-gray-100 flex-row justify-between items-center py-2 px-5 flex-1">
+              <Text className="text-lg text-primary font-semibold">
+                Packing Recommendations
+              </Text>
+            </View>
+            <View className="py-3 px-5">
+              {tripDetails.packingRecommendations.map((item, index) => {
+                return (
+                  <View
+                    key={index}
+                    className="py-1 flex-row flex-1 items-start"
+                  >
+                    <View className="rounded-full h-2 w-2 bg-gray-400 my-[5px] mr-1"></View>
+                    <Text className="text-xs text-gray-500  font-semibold ">
+                      {item}
+                    </Text>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+          <View className=" flex-wrap m-3 bg-white rounded-md border-[1px] border-gray-100 shadow-md shadow-gray-300 ">
+            <View className="border-b-[1px] border-b-gray-100 flex-row justify-between items-center py-2 px-5 flex-1">
+              <Text className="text-lg text-primary font-semibold">
+                Safety Tips
+              </Text>
+            </View>
+            <View className="py-3 px-5">
+              {tripDetails.safetyTips.map((item, index) => {
+                return (
+                  <View
+                    key={index}
+                    className="py-1 flex-row flex-1 items-start mr-1"
+                  >
+                    <View className="rounded-full h-2 w-2 bg-gray-400 my-[5px] mr-1"></View>
+                    <Text className="text-xs text-gray-500  font-semibold ">
+                      {item}
+                    </Text>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        </View>
+      ) : (
+        <View>
+          <Text>Trip details is not available</Text>
+        </View>
       )}
     </>
   );
--- front-end/store/slice/itenararySlice.ts
+++ front-end/store/slice/itenararySlice.ts
@@ -1,131 +1,118 @@
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { endpoints } from "@/constants";
-// import { Itenary } from "@/components/itenary/ItenaryBody";
-type Activity = {
-    time: string;
-    location: string;
-    description: string;
-    weatherCondition: string;
-    // weather_code: number;
-};
-type Itenary = {
-    day: number;
-    activities: Activity[];
-};
-type TripDetails = {
-    starting_location: string,
-    destination: string,
-    start_date: string,
-    end_date: string,
-    companions: string,
-    purpose: string,
-    duration: number,
-    itinerary: Itenary[],
-    forecast: {
-        time: string;
-        weather_code: number;
-        weather: string;
-    }[],
-    packingRecommendations: string[],
-    safetyTips: string[]
-}
-export const generateItinaray = createAsyncThunk(
-    "tripCast/generateItinaray",
-    async (
-        itinaray: {
-            latitude: number | undefined,
-            longitude: number | undefined,
-            companions: string,
-            destination: string,
-            start_date: string,
-            end_date: string,
-            purpose: string,
-            starting_location: string,
-            mode_of_transport: string,
-        },
-        { rejectWithValue }
-    ) => {        
-        try {
-            const response = await axios.post(
-                `${endpoints.BACKEND_URL}/ask-model`,
-                itinaray
-            );
-            console.log("respo type", typeof response.data.itinerary);
-            console.log("respo data", response.data.itinerary);
+import { Itenary } from "@/components/itenary/ItenaryBody";
 
-            
-            return response.data;
-        } catch (error: any) {
-            return rejectWithValue(error.response?.data);
-        }
+type TripDetails = {
+  starting_location: string;
+  destination: string;
+  start_date: string;
+  end_date: string;
+  companions: string;
+  purpose: string;
+  duration: number;
+  itinerary:Itenary[];
+  forecast: {
+    time: string;
+    weather_code: number;
+    weather: string;
+  }[];
+  packingRecommendations: string[];
+  safetyTips: string[];
+};
+export const generateItinaray = createAsyncThunk(
+  "tripCast/generateItinaray",
+  async (
+    itinaray: {
+      latitude: number | undefined;
+      longitude: number | undefined;
+      companions: string;
+      destination: string;
+      start_date: string;
+      end_date: string;
+      purpose: string;
+      starting_location: string;
+      mode_of_transport: string;
+    },
+    { rejectWithValue }
+  ) => {
+    console.log("req", itinaray);
+    try {
+      const response = await fetch(`${endpoints.BACKEND_URL}/ask-model`, {
+        body: JSON.stringify(itinaray),
+        method: "POST",
+      });
+      const result = await response.json();
+      return result;
+    } catch (error: any) {
+      return rejectWithValue(error.response?.data);
     }
+  }
 );
 
 export const saveItinaray = createAsyncThunk(
-    "tripCast/saveItinaray",
-    async (
-        itinaray: {
-            userId: number,
-            tripDetails: TripDetails
-        },
-        { rejectWithValue }
-    ) => {
-        try {
-            const response = await axios.post(
-                `${endpoints.BACKEND_URL}/trip`,
-                itinaray
-            );
-            return response.data;
-        } catch (error: any) {
-            return rejectWithValue(error.response?.data);
-        }
+  "tripCast/saveItinaray",
+  async (
+    itinaray: {
+      userId: number;
+      tripDetails: TripDetails;
+    },
+    { rejectWithValue }
+  ) => {
+    try {
+      const response = await axios.post(
+        `${endpoints.BACKEND_URL}/trip`,
+        itinaray
+      );
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.response?.data);
     }
+  }
 );
 
 type ItinararySlice = {
-    loading: boolean;
-    error?: object;
-    tripDetails?: TripDetails;
+  loading: boolean;
+  error?: object;
+  tripDetails?: TripDetails;
 };
 
 const initialState: ItinararySlice = {
-    loading: false,
-    error: undefined,
-    tripDetails: undefined
+  loading: false,
+  error: undefined,
+  tripDetails: undefined,
 };
 
 const itinararySlice = createSlice({
-    name: "loginData",
-    initialState,
-    reducers: {},
-    extraReducers(builder) {
-        builder.addCase(generateItinaray.pending, (state: ItinararySlice) => {
-            state.loading = true;
-        });
-        builder.addCase(generateItinaray.fulfilled, (state, action) => {
-            state.loading = false;
-            state.error = undefined;
-            state.tripDetails = action.payload.data;
-            // console.log("generate Itinaray", state.tripDetails);
-
-        });
-        builder.addCase(generateItinaray.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload ?? "";
-        });
-        builder.addCase(saveItinaray.pending, (state: ItinararySlice) => {
-            state.loading = true;
-        });
-        builder.addCase(saveItinaray.fulfilled, (state, action) => {
-            state.loading = false;
-            state.error = undefined;
-        });
-        builder.addCase(saveItinaray.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload ?? "";
-        });
-    },
+  name: "loginData",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(generateItinaray.pending, (state: ItinararySlice) => {
+      state.loading = true;
+    });
+    builder.addCase(generateItinaray.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = undefined;
+      state.tripDetails = action.payload.data;
+      // console.log("generate Itinaray", state.tripDetails);
+    });
+    builder.addCase(generateItinaray.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload ?? "";
+    });
+    builder.addCase(saveItinaray.pending, (state: ItinararySlice) => {
+      state.loading = true;
+    });
+    builder.addCase(saveItinaray.fulfilled, (state, action) => {
+      state.loading = false;
+      state.error = undefined;
+    });
+    builder.addCase(saveItinaray.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload ?? "";
+    });
+  },
 });
 
 export const itinararyActions = itinararySlice.actions;
